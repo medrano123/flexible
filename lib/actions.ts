7703c49e14ed5c6d4e3109d0b1c8f85968fa2efa
Giovanni Medrano <giovannimedrano@Giovannis-MBP.hitronhub.home>
2023-06-30 16:06:45 -0500

    grafbase

--- lib/actions.ts
+++ lib/actions.ts
@@ -8,9 +8,10 @@ const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : '
 const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
 const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
 
-
+ 
 //connection to our graphbase database
 const client = new GraphQLClient(apiUrl);
+
 
 const makeGraphQLRequest = async (query: string, variables = {}) => {
     try {
